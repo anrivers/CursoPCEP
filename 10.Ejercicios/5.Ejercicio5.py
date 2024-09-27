c8acbdb6180f6847d68d01ff0763813d39ba1bbe
anrivers <anrivers26@gmail.com>
2024-09-27 02:26:49 -0600

    Avance de ejercicios

---
+++ 10.Ejercicios/5.Ejercicio5.py
@@ -0,0 +1,28 @@
+"""
+Ejercicio básico:
+Dadas las siguientes variables:
+x = 25
+y = 15
+Utiliza operadores de comparación para comparar x e y, e imprime si:
+
+a) x es igual a y
+
+b) x es diferente de y
+
+c) x es mayor que y
+
+d) x es menor que y
+
+e) x es mayor o igual que y
+
+f) x es menor o igual que y
+"""
+x = 25
+y = 15
+
+print("x es igual a y:",x == y)
+print("x es diferente de y:",x != y)
+print("x es mayor que y:",x > y)
+print("x es menor que y:",x < y)
+print("x es mayor o igual que y:",x >= y)
+print("x es menor o igual que y:",x <= y)
