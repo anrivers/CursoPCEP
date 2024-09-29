f0a7f6de7ac4feaced246d6b03b940b97724fe59
anrivers <anrivers26@gmail.com>
2024-09-29 00:42:01 -0600

    Avance de ejercicios

---
+++ 10.Ejercicios/10.Ejercicio10.py
@@ -0,0 +1,10 @@
+"""
+Ejercicio intermedio:
+Pide al usuario que ingrese dos números enteros. 
+Utiliza operadores de comparación para determinar si el primer número es mayor que el segundo y muestra el resultado por pantalla.
+"""
+numero1 = int(input("Ingresa un numero:"))
+numero2 = int(input("Ingresa otro numero:"))
+
+comparacion1 = numero1 > numero2
+print("El primer numero es mayor?",comparacion1)
---
+++ 10.Ejercicios/9.Ejercicio9.py
@@ -0,0 +1,13 @@
+"""
+Ejercicio básico:
+
+Pide al usuario que ingrese dos números enteros y muestra por pantalla la suma y la diferencia de ambos números utilizando operadores aritméticos.
+"""
+numero1 = int(input("Ingresa un numero:"))
+numero2 = int(input("Ingresa otro numero:"))
+
+suma = numero1 + numero2
+diferencia = numero1 - numero2
+
+print("La suma es de:",suma)
+print("La diferencia es de:",diferencia)
