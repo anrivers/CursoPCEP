1f6e89decaa6008b4e3b84b5992ef31c647a6818
anrivers <anrivers26@gmail.com>
2024-10-01 22:51:36 -0600

    Avance de ejercicios

---
+++ 10.Ejercicios/13.Ejercicio13.py
@@ -0,0 +1,10 @@
+"""
+Ejercicio básico:
+Crea una cadena con tu nombre y muestra por consola el primer y último carácter de la cadena utilizando índices.
+"""
+cadena = "Andres Rivera"
+primerCaracter = cadena[0]
+segundoCaracter = cadena[-1]
+
+print("El primer caracter del nombre es:", primerCaracter)
+print("El segundo caracter del nombre es:",segundoCaracter)
---
+++ 10.Ejercicios/14.Ejercicio14.py
@@ -0,0 +1,11 @@
+"""
+Ejercicio intermedio:
+Solicita al usuario que ingrese dos palabras. 
+Luego, concatena las palabras y muestra el resultado por consola, pero separa las palabras con un guion.
+"""
+palabra1 = input("Ingresa una palabra:")
+palabra2 = input("Ingresa otra palabra:")
+
+resultado = palabra1 + " - "+ palabra2
+
+print("Las palabras son:"+ resultado)
