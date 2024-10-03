5259f1ecc3b0336d26025321ad384b5e3a655673
anrivers <anrivers26@gmail.com>
2024-10-02 23:32:38 -0600

    Avance se sección 9, listas

---
+++ 6.Estructura de Datos/1.Listas.py
@@ -0,0 +1,13 @@
+#Lista es una agrupación de datos para guardar en un solo espacio un solo tipo de dato
+frutas = ["manzana", "naranja", "fresa", "uva"]
+
+print(frutas)
+print(frutas[0])
+print(frutas[3])
+print(frutas[0:2])
+
+frutas = ["pera", "mandarina", "tomate"]
+print(frutas)
+frutas[1] = "banana"
+print(frutas)
+print(frutas[1])
