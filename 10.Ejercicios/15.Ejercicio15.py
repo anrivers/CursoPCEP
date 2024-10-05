3f3b41210b10653802e636a4a5f462cd192081c2
anrivers <anrivers26@gmail.com>
2024-10-04 19:49:31 -0600

    Avance de ejericio

---
+++ 10.Ejercicios/15.Ejercicio15.py
@@ -0,0 +1,17 @@
+"""
+Ejercicio básico modificado:
+Crea una lista con los nombres de 5 amigos y muestra el nombre del primer y último amigo utilizando índices. 
+Luego, calcula cuántos caracteres tienen esos nombres y muestra los resultados por consola.
+"""
+amigos = ['zezar', 'ever', 'diego', 'andres', 'kebin']
+
+print(amigos)
+
+primer_amigo = amigos[0]
+segundo_amigo = amigos[-1]
+
+print(primer_amigo)
+print(segundo_amigo)
+
+print(len(primer_amigo))
+print(len(segundo_amigo))
