26290674770de0966e3fce6c808724dd277ba6a5
anrivers <anrivers26@gmail.com>
2024-10-04 19:50:39 -0600

    Ejercicio sin terminar

---
+++ 10.Ejercicios/16.Ejercicio16.py
@@ -0,0 +1,6 @@
+"""
+Ejercicio intermedio modificado:
+Solicita al usuario que ingrese cinco números enteros separados por espacios. 
+Crea una lista con los números ingresados y calcula el promedio de los números en la lista.
+"""
+
