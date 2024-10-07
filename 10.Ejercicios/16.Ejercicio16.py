a6dd8d24b87e23acc7117923931d68f4c9c0a4b9
anrivers <anrivers26@gmail.com>
2024-10-07 16:20:01 -0600

    Ejercicio 16

--- 10.Ejercicios/16.Ejercicio16.py
+++ 10.Ejercicios/16.Ejercicio16.py
@@ -3,4 +3,12 @@ Ejercicio intermedio modificado:
 Solicita al usuario que ingrese cinco números enteros separados por espacios. 
 Crea una lista con los números ingresados y calcula el promedio de los números en la lista.
 """
+numeros = input("Por favor, ingresa cinco numeros separados por espacios:")
 
+listaNumeros = list(map(int, numeros.split()))
+print(listaNumeros)
+
+suma = sum(listaNumeros)
+promedio = suma / len(listaNumeros)
+
+print("El promedio de los numeros ingresados es de:", promedio)
