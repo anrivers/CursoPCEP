4c99d7cfde4577f2fd2b607bd780b1836d98a5b6
anrivers <anrivers26@gmail.com>
2024-10-10 02:33:15 -0600

    Avance de la sección 9

---
+++ 6.Estructura de Datos/7.TuplasListas.py
@@ -0,0 +1,10 @@
+personas = [("Juan" , 25 , "M") , ("Maria" , 34 , "F") , ("Pedro" , 19 , "M")]
+
+print(personas)
+print(personas[0])
+print(personas[2])
+print(personas[1][0])
+print(personas[0][1])
+
+personas[0] = ("Walter" , 20 , "M")
+print(personas)
