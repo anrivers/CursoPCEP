b4b5399ebf6bd4216c97701a199e53d607d18481
anrivers <anrivers26@gmail.com>
2024-10-17 21:45:50 -0600

    Avance de la sección 10

---
+++ 7.Estructuras de control/2.CondicionalesMultiples.py
@@ -0,0 +1,14 @@
+calificaciones = 150
+
+if calificaciones >= 90 and calificaciones < 100:
+    print("Calificación excelente")
+elif calificaciones >= 80 and calificaciones < 100:
+    print("Calificación muy buena")
+elif calificaciones >= 70 and calificaciones < 100:
+    print("Calificación buena")
+elif calificaciones >= 60 and calificaciones < 100:
+    print("Calificación suficiente")
+elif calificaciones <= 59 and calificaciones < 100:
+    print("Calificación insuficinete")
+else:
+    print("Calificacion inexistente")
