b3ed2395668b471b6b5bf2358c67d838ef29953f
anrivers <anrivers26@gmail.com>
2024-10-23 22:47:45 -0600

    Avance de la sección 10

---
+++ 7.Estructuras de control/6.For.py
@@ -0,0 +1,13 @@
+nombres = ["Ana", "Carlos", "Pedro"]
+
+for nombre in nombres:
+    print("Nombre:", nombre)
+    
+texto = "Python"
+
+for i in texto:
+    print("Letra es:", i)
+   
+    
+for j in range(1 , 11):
+    print(j)  
