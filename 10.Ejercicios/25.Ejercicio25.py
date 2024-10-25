c66585933d6ff6b01cb7954f25916795e64722a0
anrivers <anrivers26@gmail.com>
2024-10-24 23:53:23 -0600

    Avance de los ejercicios de la sección 10

---
+++ 10.Ejercicios/25.Ejercicio25.py
@@ -0,0 +1,9 @@
+"""
+Ejercicio básico:
+Pide al usuario que ingrese su nombre. Utiliza un bucle for para mostrar por consola cada letra del nombre en una línea separada.
+"""
+
+nombres = input("Ingresa tu nombre:")
+
+for nombre in nombres:
+    print("Nombre:", nombre)
