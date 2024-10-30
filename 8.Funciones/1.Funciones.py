89832b8766d4e0b0e61cf705e30254441d412438
anrivers <anrivers26@gmail.com>
2024-10-29 22:52:58 -0600

    Avance de la sección 11

---
+++ 8.Funciones/1.Funciones.py
@@ -0,0 +1,8 @@
+num1 = 10
+num2 = 20
+
+def sumar():
+    suma = num1 + num2
+    print(suma) 
+    
+sumar()
---
+++ 8.Funciones/2.ParametrosArgumentos.py
@@ -0,0 +1,11 @@
+def saludo(nombre):
+    print(f"Hola, {nombre}")
+    
+saludo("Andres Rivera")
+saludo("Stephen Curry")
+
+def suma(a, b):
+    resultado = a + b
+    print(f"La suma de {a} y {b} es: {resultado}")
+
+suma(10,5)
