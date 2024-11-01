c9e6d9c4f70c069480f728b56b991e230c57fe3c
anrivers <anrivers26@gmail.com>
2024-10-31 21:51:29 -0600

    Avance de la sección 11

---
+++ 8.Funciones/3.PosicionesParametrosArgumentos.py
@@ -0,0 +1,14 @@
+def saludar(nombre, mensaje):
+    print(f"{mensaje}, {nombre}")
+    
+saludar("Andres","Hola")
+
+def saludo(nombre, mensaje):
+    print(f"Tu nombre es {nombre} y el mensaje que transmites es: {mensaje}")
+    
+saludo(mensaje="Bievenidos al himalayaaaa", nombre="Andres Rivera")
+
+def saludamos(nombre, mensaje, fecha):
+    print(f"Hola {nombre}, hoy es: {fecha} y tu mensaje es: {mensaje}")
+    
+saludamos("Andres Rivera", fecha="03/10/2024", mensaje="Bienvenido")
---
+++ 8.Funciones/4.ValoresPredeteminados.py
@@ -0,0 +1,5 @@
+def saludar(nombre, mensaje="Hola mundo"):
+    print(f"{mensaje}, {nombre}")
+
+saludar("Andres Rivera")
+saludar("Andres Rivera", "Adios")
