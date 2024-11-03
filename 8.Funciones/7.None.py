8ba3d36c675a5fc0c1691ea2b40dd15aa1642fa0
anrivers <anrivers26@gmail.com>
2024-11-02 22:59:04 -0600

    Avance de la sección 11

---
+++ 8.Funciones/7.None.py
@@ -0,0 +1,13 @@
+num1 = 0
+num2 = None
+
+if num2 == None:
+    print("Este valor está vacío")
+else:
+    print("Este valor tiene un valor")
+    
+def imprimirMensaje(mensaje):
+    print(mensaje)
+
+resultado = imprimirMensaje("hola mundo")
+print(resultado)
